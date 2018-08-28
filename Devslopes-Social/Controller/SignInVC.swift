094441c3cefc2124be18e47453bc65cd283b47c4
Araz Sinjary <araz@Arazs-MBP.Home>
2018-08-28 21:26:37 +0300

    setup firebase

---
+++ Devslopes-Social/Controller/SignInVC.swift
@@ -0,0 +1,21 @@
+//
+//  SignInVC.swift
+//  Devslopes-Social
+//
+//  Created by Araz Sinjary on 8/28/18.
+//  Copyright © 2018 Araz Sinjary. All rights reserved.
+//
+
+import UIKit
+
+class SignInVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    
+
+
+}
+
